045d72a81f7e8029bd4b83b39d8d082e1115982e
unknown <sachin.sharma0583@gmail.com>
2022-01-21 13:01:39 +0530

    First commit

---
+++ Sales_Inventory/Sales_Inventory/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Sales_Inventory.DAL;
+using Sales_Inventory.Models;
+
+namespace Sales_Inventory.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Variable
+        DBWorker worker = new DBWorker();
+        #endregion
+
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                UserViewModel user = VerifyLogin(model.Email, model.Password);
+                if(user != null)
+                {
+                    if(user.IsActive)
+                    {
+                        Session["UserId"] = user.UserId;
+                        Session["Email"] = user.Email;
+                        Session["Fullname"] = user.Fullname;
+                        Session["RoleId"] = user.RoleId;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Your account is inactive. Please contact to administrator.");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User Name or Password is Invalid.");
+                }
+                return View(model);
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+
+        public UserViewModel VerifyLogin(string Email, string Password)
+        {
+            UserViewModel model = new UserViewModel();
+            if(Email != "" && Password != "")
+            {
+                var user = worker.UserEntity.Get(x => x.Email == Email && x.Password == Password).FirstOrDefault();
+                model.Email = user.Email;
+                model.Fullname = user.FullName;
+                model.UserId = user.UserId;
+                model.RoleId = user.RoleId;
+                model.IsActive = user.IsActive;
+            }
+            return model;
+        }
+    }
+}
---
+++ Sales_Inventory/Sales_Inventory/Models/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sales_Inventory.Models
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+        public int? RoleId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Fullname { get; set; }
+        public string PhoneNo { get; set; }
+        public string Address { get; set; }
+        public int State { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+        public bool IsActive { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<System.DateTime> UpdatedDate { get; set; }
+    }
+}
