138d2fd88c9d939ab22e5283dfcfbfb153aaa882
SachinSharma05 <sachin.sharma0583@gmail.com>
2022-03-09 15:59:51 +0530

    Advance Model Update

--- Sales_Inventory/DAL/Advance.cs
+++ Sales_Inventory/DAL/Advance.cs
@@ -11,27 +11,16 @@
 
 namespace Sales_Inventory.DAL
 {
-
-using System;
-    using System.Collections.Generic;
-    
-public partial class Advance
-{
-
-    public int Id { get; set; }
-
-    public string Advance_To { get; set; }
-
-    public Nullable<System.DateTime> Advance_Date { get; set; }
-
-    public string Advance_Amount { get; set; }
-
-    public string Advance_Against { get; set; }
-
-    public Nullable<int> CreatedBy { get; set; }
-
-    public Nullable<System.DateTime> CreatedDate { get; set; }
-
-}
-
+    using System;
+    using System.Collections.Generic;  
+    public partial class Advance
+    {
+        public int Id { get; set; }
+        public string Advance_To { get; set; }
+        public Nullable<System.DateTime> Advance_Date { get; set; }
+        public string Advance_Amount { get; set; }
+        public string Advance_Against { get; set; }
+        public Nullable<int> CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+    }
 }
